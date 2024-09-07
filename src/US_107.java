487fb0a733a68aa4f818f6424c88c817443852ae
UmmuhanTeke <ummuhantekeee@gmail.com>
2024-09-07 03:18:29 +0300

    US_107 birinci Test Case tamamlanmıştır. Kolay gelsin.

---
+++ src/US_107.java
@@ -0,0 +1,61 @@
+import Utility.BaseDriver;
+import Utility.MyFunc;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class US_107 extends BaseDriver {
+    String emailStr="team6@gmail.com";
+    String passwordStr="Team61234";
+
+    @Test
+    public void accountDeletePositiveTest()
+    {
+        driver.get("https://www.akakce.com/");
+        MyFunc.Wait(2);
+
+        WebElement login=driver.findElement(By.xpath("(//a[text()='Giriş Yap'])[1]"));
+        login.click();
+        MyFunc.Wait(2);
+
+        WebElement email=driver.findElement(By.xpath("//input[@class='t' and @name='life']"));
+        email.sendKeys(emailStr);
+        MyFunc.Wait(2);
+
+        WebElement password=driver.findElement(By.xpath("(//input[@type='password'])[1]"));
+        password.sendKeys(passwordStr);
+        MyFunc.Wait(2);
+
+        WebElement loginButton=driver.findElement(By.xpath("(//input[@value='Giriş yap'])[1]"));
+        loginButton.click();
+        MyFunc.Wait(2);
+
+        WebElement myProfile=driver.findElement(By.xpath("(//a[@rel='nofollow'])[1]"));
+        myProfile.click();
+        MyFunc.Wait(2);
+
+        WebElement deleteAccount=driver.findElement(By.xpath("//a[text()='Hesabımı Sil']"));
+        deleteAccount.click();
+        MyFunc.Wait(2);
+
+        WebElement passwordBox=driver.findElement(By.xpath("//input[@name='p']"));
+        passwordBox.click();
+        MyFunc.Wait(2);
+
+        WebElement passwordWrite=driver.findElement(By.xpath("//input[@name='p']"));
+        passwordWrite.sendKeys(passwordStr);
+        MyFunc.Wait(2);
+
+        WebElement accountDelete=driver.findElement(By.xpath("//input[@type='submit']"));
+        accountDelete.click();
+        MyFunc.Wait(2);
+
+        WebElement deleteAccountControl=driver.findElement(By.xpath("//ul[@class='cul']//p//i"));
+        Assert.assertTrue("Hesabın Silinemedi", deleteAccountControl.getText().contains("Hesabın silindi"));
+        MyFunc.Wait(2);
+
+        tearDown();
+    }
+}
+
